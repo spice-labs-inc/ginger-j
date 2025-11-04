79e61b799e2d7fefeabbf064f757458e4fe7ca89
David Pollak <dpp@spicelabs.io>
2025-11-04 12:36:23 -0500

    Added Bouncy Castle and support for encrypting files more than 2GB in size
    Signed-off-by: David Pollak <dpp@spicelabs.io>

--- src/test/java/io/spicelabs/ginger/CryptoUtilTest.java
+++ src/test/java/io/spicelabs/ginger/CryptoUtilTest.java
@@ -4,12 +4,18 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.nio.file.*;
+import java.security.SecureRandom;
+import java.io.*;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import java.security.Security;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CryptoUtilTest {
   @Test
   void aesGcmEncryptProducesData() throws Exception {
+    Security.addProvider(new BouncyCastleProvider());
     byte[] key = CryptoUtil.generateAesKey();
     byte[] iv = CryptoUtil.generateIv();
     byte[] plain = "hi".getBytes();
@@ -17,5 +23,43 @@ class CryptoUtilTest {
     var out = new ByteArrayOutputStream();
     CryptoUtil.aesGcmEncrypt(key, iv, in, out);
     assertTrue(out.size() > plain.length);
+  }
+
+  @Test
+  void testLargeEncryption() throws Exception {
+    Security.addProvider(new BouncyCastleProvider());
+    Path temp = Files.createTempFile("ginger_", ".test");
+    try {
+      OutputStream fos = new FileOutputStream(temp.toFile());
+      byte[] bytes = new byte[4096];
+SecureRandom r1 = new SecureRandom();
+
+
+for (int x = 0; x < 2000000; x++) {
+  r1.nextBytes(bytes);
+  fos.write(bytes);
+}
+
+fos.flush();
+fos.close();
+    byte[] key = CryptoUtil.generateAesKey();
+    byte[] iv = CryptoUtil.generateIv();
+    Path tempOut = Files.createTempFile("ginger_", ".test");
+
+    try {
+      fos = new FileOutputStream(tempOut.toFile());
+      InputStream fis = new FileInputStream(temp.toFile());
+      CryptoUtil.aesGcmEncrypt(key, iv, fis, fos);
+      fos.flush();
+      fos.close();
+      // the output should be longer than the input
+      assertTrue(temp.toFile().length() < tempOut.toFile().length());
+    } finally {
+      tempOut.toFile().delete();
+    }
+
+    } finally {
+      temp.toFile().delete();
+    }
   }
 }
